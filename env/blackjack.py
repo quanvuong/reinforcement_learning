244b906b4554611ecf8236f88e91c4fea73a5595
quanvuong <quan.vuong@nyu.edu>
2017-05-14 17:10:54 +0400

    add class function placeholder for blackjack

--- env/blackjack.py
+++ env/blackjack.py
@@ -8,3 +8,18 @@ class Blackjack(EnvAbstract, BlackjackEnv):
     def __init__(self, **kwargs):
         self.spec = None
         super().__init__(kwargs)
+
+    def get_possible_actions(self, state):
+        pass
+
+    def get_possible_next_states(self, state, action):
+        pass
+
+    def get_trans_prob(self, state, action, ns):
+        pass
+
+    def get_reward(self, state, action, ns):
+        pass
+
+    def get_state_space(self):
+        pass
